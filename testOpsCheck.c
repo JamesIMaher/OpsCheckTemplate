5bfc46569b6496201eeeaede9fcb42b2047528d8
JamesIMaher <70163214+JamesIMaher@users.noreply.github.com>
2021-01-10 22:02:05 -0700

    Update testOpsCheck.c

--- testOpsCheck.c
+++ testOpsCheck.c
@@ -4,12 +4,12 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
 
 int main (int argc, char *argv[]){
     char* newString = HelloWorldFunc();
     char* checkString = malloc(sizeof("Hello World!"));
-    checkString = newString;
-    //checkString = "Oops";
+    strcpy(checkString, "Hello World!");
     assert(checkString == newString && "You have an error in how you return the string from HelloWorldFunc to the main program.");
 }
 //Simple
